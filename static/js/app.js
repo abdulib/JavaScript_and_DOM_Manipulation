cbcc6c223a21c73f95a0d67046a453ae9653430c
abdulib <abdulrahmon.ibrahim@yahoo.com>
2019-05-17 19:27:41 -0500

    Uploading Table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,35 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// test
+// console.log(Object.entries(tableData[0]));
+
+tableData.splice(2,1, {
+    datetime: "1/28/1996",
+    city: "dallas",
+    state: "tx",
+    country: "us",
+    shape: "star",
+    durationMinutes: "5 mins.",
+    comments: "Cowboys win a superbowl, that's alien!."
+    });
+
+console.log(Object.entries(tableData[2]));
+
+
+var table = d3.select("table")
+
+var body = d3.select("tbody")
+
+
+tableData.forEach((sighting) => {
+    var newrow = body.append('tr');
+
+    Object.entries(sighting).forEach(([key, value]) => {
+        // console.log(key, value)
+        
+        newrow.append('td').text(value)
+  
+    });
+});
+
